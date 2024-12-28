8e44f98a6daa43d6ccc263ffbb0e78ae236ba9e3
unknown <ogunladestephen20@gmail.com>
2024-12-28 01:11:45 +0100

    Code: Updated chat app location

--- src/components/AuthProvider/AuthProvider.jsx
+++ src/components/AuthProvider/AuthProvider.jsx
@@ -1,9 +1,24 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import React from "react";
+import React, { useEffect } from "react";
 
 const AuthProvider = ({ children }) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://embed.tawk.to/676f2886af5bfec1dbe36819/1ig53ck6t";
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+
+    const firstScript = document.getElementsByTagName("script")[0];
+    firstScript.parentNode.insertBefore(script, firstScript);
+
+    return () => {
+      // Cleanup: Remove the script when the component unmounts
+      script.parentNode?.removeChild(script);
+    };
+  }, []);
   return <SessionProvider>{children}</SessionProvider>;
 };
 
